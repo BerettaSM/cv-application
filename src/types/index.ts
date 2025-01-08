ec0173e4e113449e3ebed9750e0b5be8fb2e23b7
Ramon Saviato <ramonsaviato@hotmail.com>
2025-01-07 21:24:22 -0300

    add types to describe a resume

---
+++ src/types/index.ts
@@ -0,0 +1,42 @@
+export interface Socials {
+  phone: string;
+  email: string;
+  website: string;
+  github: string;
+  linkedIn: string;
+}
+
+export interface BulletPoint {
+  bold: string | null;
+  text: string;
+}
+
+export interface SimpleEntry {
+  title: string;
+  description: string;
+}
+
+export interface YearEntry extends SimpleEntry {
+  year: number;
+  location: string;
+}
+
+export interface DetailedEntry extends SimpleEntry {
+  timeline: string;
+  location: string;
+  bulletPoints: BulletPoint[];
+}
+
+export interface Resume {
+  name: string;
+  address: string;
+  socials: Socials;
+  profile: string;
+  education: DetailedEntry[];
+  experience: DetailedEntry[];
+  projects: DetailedEntry[];
+  skills: SimpleEntry[];
+  achievements: YearEntry[];
+  interests: SimpleEntry[];
+  languages: SimpleEntry[];
+}
